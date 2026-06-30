9da7e3768c2ed871ff97ec183e29bd52a689169a
jayarathinam30 <160031428+jayarathinam30@users.noreply.github.com>
2026-06-30 10:29:05 +0530

    Qadence publish: TC-006 search process

---
+++ testscript/SearchProcess/TC-006_search_process.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+test('search process', {
+  annotation: { type: 'QADENCE_TC_ID', description: 'TC-006' },
+  tag: ['@QADENCE_TC_ID:TC-006']
+}, async ({ page, request }) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login');
+
+  // Step 1: click
+  await page.getByLabel("Email address *").click();
+
+  // Step 2: fill
+  await page.getByLabel("Email address *").fill('asdfe');
+
+  // Step 3: click
+  await page.getByRole("textbox", { name: "Password *" }).click();
+
+  // Step 4: fill
+  await page.getByRole("textbox", { name: "Password *" }).fill('werwer');
+
+  // Step 5: click
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Step 6: click
+  await page.getByRole("link", { name: "Home" }).click();
+});
+
