433fe8819120085b6128a1a6f4311abd5774bd82
jayarathinam30 <160031428+jayarathinam30@users.noreply.github.com>
2026-06-30 10:21:10 +0530

    Qadence publish: TC-005 prodouct login

---
+++ testscript/Prodouct/TC-005_prodouct_login.spec.ts
@@ -0,0 +1,33 @@
+import { test, expect } from '@playwright/test';
+
+test('prodouct login', {
+  annotation: { type: 'QADENCE_TC_ID', description: 'TC-005' },
+  tag: ['@QADENCE_TC_ID:TC-005']
+}, async ({ page, request }) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login');
+
+  // Step 1: click
+  await page.getByLabel("Email address *").click();
+
+  // Step 2: fill
+  await page.getByLabel("Email address *").fill('asdfewr');
+
+  // Step 3: click
+  await page.getByRole("textbox", { name: "Password *" }).click();
+
+  // Step 4: fill
+  await page.getByRole("textbox", { name: "Password *" }).fill('qwere');
+
+  // Step 5: click
+  await page.getByRole("button", { name: "Login" }).click();
+
+  // Step 6: click
+  await page.getByRole("link", { name: "Contact" }).click();
+
+  // Step 7: click
+  await page.getByRole("textbox", { name: "First name" }).click();
+
+  // Step 8: fill
+  await page.getByRole("textbox", { name: "First name" }).fill('werew');
+});
+
