9c29801485308f72332734b7988318e21fb860db
jayarathinam30 <160031428+jayarathinam30@users.noreply.github.com>
2026-07-01 19:03:41 +0530

    Qadence publish: TC-012 abcdef

---
+++ test/Abcd1/TC-012_abcdef.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('abcdef', {
+  annotation: { type: 'QADENCE_TC_ID', description: 'TC-012' },
+  tag: ['@QADENCE_TC_ID:TC-012']
+}, async ({ page, request }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  // Step 1: click
+  await page.locator('[data-test="username"]').click();
+
+  // Step 2: fill
+  await page.locator('[data-test="username"]').fill('dfsdf');
+
+  // Step 3: click
+  await page.locator('[data-test="password"]').click();
+
+  // Step 4: fill
+  await page.locator('[data-test="password"]').fill('sdfdf');
+
+  // Step 5: click
+  await page.locator('[data-test="login-button"]').click();
+});
+
