5e47cf32be0aa5bd956b386fac6d3e9c60b26f4e
jayarathinam30 <160031428+jayarathinam30@users.noreply.github.com>
2026-07-03 18:08:46 +0530

    Refactor Checkout Process test steps
    Refactored test steps for better clarity and organization.

--- tests/Checkout/TC-004_Checkout_Process.spec.ts
+++ tests/Checkout/TC-004_Checkout_Process.spec.ts
@@ -1,21 +1,32 @@
 import { test, expect } from '@playwright/test';
 
-test('Checkout Process', {
-  annotation: { type: 'QADENCE_TC_ID', description: 'TC-004' },
-  tag: ['@QADENCE_TC_ID:TC-004']
-}, async ({ page, request }) => {
-  await page.goto('https://practicesoftwaretesting.com/auth/login');
+test(
+  'Checkout Process',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-004' },
+    tag: ['@QADENCE_TC_ID:TC-004'],
+  },
+  async ({ page }) => {
 
-  // Step 1: click
-  await page.getByRole("link", { name: "Contact" }).click();
+    await test.step('Open Login Page', async () => {
+      await page.goto('https://practicesoftwaretesting.com/auth/login');
+    });
 
-  // Step 2: click
-  await page.locator('[data-test="first-name"]').click();
+    await test.step('Click Contact Link', async () => {
+      await page.getByRole('link', { name: 'Contact' }).click();
+    });
 
-  // Step 3: click
-  await page.locator('[data-test="last-name"]').click();
+    await test.step('Click First Name Field', async () => {
+      await page.locator('[data-test="first-name"]').click();
+    });
 
-  // Step 4: click
-  await page.locator('[data-test="contact-submit"]').click();
-});
+    await test.step('Click Last Name Field', async () => {
+      await page.locator('[data-test="last-name"]').click();
+    });
 
+    await test.step('Click Submit Button', async () => {
+      await page.locator('[data-test="contact-submit"]').click();
+    });
+
+  }
+);
