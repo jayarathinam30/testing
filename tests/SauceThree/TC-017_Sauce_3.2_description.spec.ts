f2efacbce77d553b0926acbb73a60a0fad449a75
jayarathinam30 <160031428+jayarathinam30@users.noreply.github.com>
2026-07-09 09:47:42 +0530

    Add test for Sauce 3.2 login functionality
    This test script automates the login process for Sauce 3.2 using Playwright.

---
+++ tests/SauceThree/TC-017_Sauce_3.2_description.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from '@playwright/test';
+
+test(
+  'Sauce 3.2 description',
+  {
+    annotation: { type: 'QADENCE_TC_ID', description: 'TC-017' },
+    tag: ['@QADENCE_TC_ID:TC-017'],
+  },
+  async ({ page }) => {
+
+
+    await test.step('Open page', async () => {
+      await page.goto('https://www.saucedemo.com/');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="username"]').click();
+    });
+
+    await test.step('fill', async () => {
+      await page.locator('[data-test="username"]').fill('tandard-user');
+    });
+
+    await test.step('fill', async () => {
+      await page.locator('[data-test="password"]').fill('secret_sauce');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="login-button"]').click();
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="login-container"]').click();
+    });
+
+    await test.step('fill', async () => {
+      await page.locator('[data-test="username"]').fill('standard-user');
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="login-button"]').click();
+    });
+
+    await test.step('click', async () => {
+      await page.locator('[data-test="login-container"]').click();
+    });
+
+  }
+);
